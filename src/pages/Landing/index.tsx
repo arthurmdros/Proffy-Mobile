ef5a6f0e43752a7a6834b3630759615ab612162b
arthurmdros <arthurmedeiros2005@gmail.com>
2020-08-07 14:15:21 -0300

    Importando navegação para a rota da GiveClasses Screen

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { View, Text, Image, TouchableOpacity } from 'react-native';
+import { View, Text, Image } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 import styles from './styles';
 
@@ -9,26 +11,33 @@ import giveClassesIcon from '../../assets/images/icons/give-classes.png';
 import heartIcon from '../../assets/images/icons/heart.png';
 
 function Landing(){
+
+    const { navigate } = useNavigation();
+
+    function handleNavigateToGiveClassesScreen(){
+        navigate('GiveClasses');
+    }
+
     return (
         <View style={styles.container}>
-            <Image source={landingImg} />
+            <Image source={landingImg} style={styles.banner}/>
             <Text style={styles.title}>
                 Seja bem-vindo, {'\n'}
                 <Text style={styles.titleBold}>O que deseja fazer?</Text>
             </Text>
 
             <View style={styles.buttonsContainer}>
-                <TouchableOpacity style={[styles.button, styles.buttonPrimary]}>
+                <RectButton style={[styles.button, styles.buttonPrimary]}>
                     <Image source={studyIcon} />
                     
                     <Text style={styles.buttonText}>Estudar</Text>                    
-                </TouchableOpacity>
+                </RectButton>
 
-                <TouchableOpacity style={[styles.button, styles.buttonSecondary]}>
+                <RectButton onPress={handleNavigateToGiveClassesScreen} style={[styles.button, styles.buttonSecondary]}>
                     <Image source={giveClassesIcon} />
                     
                     <Text style={styles.buttonText}>Dar aulas</Text>                    
-                </TouchableOpacity>
+                </RectButton>
             </View>
 
             <Text style={styles.totalConnections}>
