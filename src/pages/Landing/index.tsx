4542f407e5a4ac340cc243f510c694b56bb3fbaa
arthurmdros <arthurmedeiros2005@gmail.com>
2020-08-07 10:49:55 -0300

    Inicializando screen landing

---
+++ src/pages/Landing/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import styles from './styles';
+
+function Landing(){
+    return (
+        <View style={styles.container}>
+            <Text>Landing Screen</Text>      
+        </View>
+    );
+}
+
+export default Landing;
