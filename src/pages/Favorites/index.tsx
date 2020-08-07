2332a9ef53fee6b840a4cca934406048bb0f3bc6
arthurmdros <arthurmedeiros2005@gmail.com>
2020-08-07 15:57:01 -0300

    Importando o componente de cabeçalho na Favorites Screen

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import PageHeader from '../../components/PageHeader';
 
 import styles from './styles';
 
 function Favorites(){
     return(
         <View style={styles.container}>
-            <Text style={styles.title}>Favorites Screen</Text>
+            <PageHeader title="Meus proffys favoritos" />
         </View>
     );
 }
