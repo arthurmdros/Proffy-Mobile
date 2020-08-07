fbe5155039c9605f091114541d9748db3f8735e4
arthurmdros <arthurmedeiros2005@gmail.com>
2020-08-07 14:36:58 -0300

    Adicionando estilização dos elementos finais da GiveClasses Screen

--- src/pages/GiveClasses/styles.ts
+++ src/pages/GiveClasses/styles.ts
@@ -8,6 +8,10 @@ const styles = StyleSheet.create({
       justifyContent: 'center',
     },  
 
+    content: {
+        flex: 1,
+        justifyContent: 'center'
+    },
     
     title: {
         fontFamily: 'Poppins_400Regular',
@@ -15,7 +19,31 @@ const styles = StyleSheet.create({
         fontSize: 20,
         lineHeight: 30,
         marginTop: 80,
-    }
+    },
+
+    description: {
+        marginTop: 24,
+        color: '#d4c2ff',
+        fontSize: 16,
+        lineHeight: 26,
+        fontFamily: 'Poppins_400Regular',
+        maxWidth: 240
+    },
+
+    okButton: {
+        marginVertical: 40,
+        backgroundColor: '#04d361',
+        height: 58,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 8
+    },
+
+    okButtonText: {
+        color: '#FFF',
+        fontSize: 16,
+        fontFamily: 'Archivo_700Bold'
+    },
 
 });
 
