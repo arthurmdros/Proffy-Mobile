43a6bc0289537e9a7dfef9c982857f7286517992
arthurmdros <arthurmedeiros2005@gmail.com>
2020-08-07 14:59:21 -0300

    Inicializando estilização da TeacherList Screen

---
+++ src/pages/TeacherList/styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#8257E5',
+      padding: 40,
+      justifyContent: 'center',
+    },  
+
+    title: {
+        fontFamily: 'Poppins_400Regular',
+        color: '#FFF',
+        fontSize: 20,
+        lineHeight: 30,
+        marginTop: 80,
+    }
+});
+
+export default styles;
