0211712f7a43c51a39e4107241515951088c53b7
arthurmdros <arthurmedeiros2005@gmail.com>
2020-08-07 14:36:40 -0300

    Adicionando elementos finais na GiveClasses Screen

--- src/pages/GiveClasses/index.tsx
+++ src/pages/GiveClasses/index.tsx
@@ -1,13 +1,34 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { View, Text, ImageBackground } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+
+import giveClassesBgImage from '../../assets/images/give-classes-background.png';
 import styles from './styles';
 
 function GiveClasses(){ 
+    const { goBack } = useNavigation();
+
+    function handleNavigateToBack(){
+        goBack();
+    }   
+
     return(
         <View style={styles.container}>
-            <Text style={styles.title}>
-                Give Classes Screen
-            </Text>
+            <ImageBackground
+                resizeMode="contain"
+                source={giveClassesBgImage}
+                style={styles.content}
+            >
+                <Text style={styles.title}> Desejar ser um Proffy? </Text>
+                <Text style={styles.description}>
+                    Para começar, você precisa se cadastrar como professor na nossa plataforma web.
+                </Text>
+            </ImageBackground>
+
+            <RectButton onPress={handleNavigateToBack} style={styles.okButton}>
+                <Text style={styles.okButtonText}> Tudo bem </Text>
+            </RectButton>
         </View>
     );
 }
