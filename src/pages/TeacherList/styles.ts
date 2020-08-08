6cf6ba6875634382bf8df95c242d4ce39fb44601
arthurmdros <arthurmedeiros2005@gmail.com>
2020-08-08 13:47:45 -0300

    Removendo estilizações desncessárias no container

--- src/pages/TeacherList/styles.ts
+++ src/pages/TeacherList/styles.ts
@@ -3,9 +3,7 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#8257E5',
-      padding: 40,
-      justifyContent: 'center',
+      backgroundColor: '#FFF',      
     },  
 
     title: {
