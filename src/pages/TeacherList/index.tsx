52f1e79cdd05540f2c0f801bf2b6f2aa7820a036
arthurmdros <arthurmedeiros2005@gmail.com>
2020-08-09 13:05:46 -0300

    Adicionando requisição da API da filtragem de professores

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -3,16 +3,35 @@ import { View, ScrollView, Text, TextInput } from 'react-native';
 import { BorderlessButton, RectButton } from 'react-native-gesture-handler';
 import { Feather } from '@expo/vector-icons';
 import PageHeader from '../../components/PageHeader';
-import TeacherItem from '../../components/TeacherItem';
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
 
 
 import styles from './styles';
+import api from '../../services/apip';
 
 function TeacherList(){
+    const [teachers, setTeachers] = useState([]);
     const [isFiltersVisible, setIsFiltersVisible] = useState(false);
+
+    const [subject, setSubject] = useState('');
+    const [week_day, setWeekDay] = useState('');
+    const [time, setTime] = useState('');
 
     function handleToggleFiltersVisible() {
         setIsFiltersVisible(!isFiltersVisible);
+    }
+
+    async function handleFiltersSubmit() {
+        const response = await api.get('classes', {
+            params: {
+                subject,
+                week_day,
+                time,
+            }
+        });
+
+        setIsFiltersVisible(false);
+        setTeachers(response.data);
     }
 
     return(
@@ -31,31 +50,37 @@ function TeacherList(){
                             <Text style={styles.label}>Matéria</Text>
                             <TextInput 
                                 style={styles.input}
+                                value={subject}
+                                onChangeText={text => setSubject(text)}
                                 placeholder="Qual a matéria?"
                                 placeholderTextColor="#c1bccc"
                             />
 
                         <View  style={styles.inputGroup}>
                             <View style={styles.inputBlock}>
-                                <Text style={styles.label}>Matéria</Text>
+                                <Text style={styles.label}>Dia da semana</Text>
                                 <TextInput 
                                     style={styles.input}
+                                    value={week_day}
+                                    onChangeText={text => setWeekDay(text)}
                                     placeholder="Qual o dia?"
                                     placeholderTextColor="#c1bccc"
                                 />
                             </View>
 
                             <View style={styles.inputBlock}>
-                                <Text style={styles.label}>Matéria</Text>
+                                <Text style={styles.label}>Horário</Text>
                                 <TextInput 
                                     style={styles.input}
+                                    value={time}
+                                    onChangeText={text => setTime(text)}
                                     placeholder="Qual o horário?"
                                     placeholderTextColor="#c1bccc"
                                 />
                             </View>
                         </View> 
 
-                        <RectButton style={styles.filterButton}>
+                        <RectButton onPress={handleFiltersSubmit} style={styles.filterButton}>
                             <Text style={styles.filterButtonText}>Filtrar</Text>
                         </RectButton>
                     </View>                    
@@ -69,11 +94,9 @@ function TeacherList(){
                     paddingBottom: 16,
                 }}
             >
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
+                {teachers.map(( teacher: Teacher) => {
+                    return <TeacherItem key={teacher.id} teacher={teacher} />
+                })}
             </ScrollView>
         </View>
     );
