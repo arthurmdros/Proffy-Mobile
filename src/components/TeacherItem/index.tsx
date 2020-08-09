d6eb1ac658a30313e2dde94e95fda198109277d1
arthurmdros <arthurmedeiros2005@gmail.com>
2020-08-09 13:09:33 -0300

    Adicionando link para contato pelo WhatsApp

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Image, Text } from 'react-native';
+import { View, Image, Text, Linking } from 'react-native';
 
 import styles from './styles';
 
@@ -23,6 +23,10 @@ export interface TeacherItemProps {
 }
 
 const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function handleLinkToWhatsapp() {
+        Linking.openURL(`whatsapp://send?phone=${teacher.whatsapp}`);
+    }
+
     return (
         <View style={styles.container}>
             <View style={styles.profile}>
@@ -50,7 +54,7 @@ const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
                         <Image source={unfavoriteIcon} />
                     </RectButton>
 
-                    <RectButton style={styles.contactButton}>
+                    <RectButton onPress={handleLinkToWhatsapp} style={styles.contactButton}>
                         <Image source={whatsappIcon}/>
                         <Text style={styles.contactButtonText}>Entrar em contato</Text>
                     </RectButton>
